56e8ca6b66c1f978dbba0fe1d20a7b629a3b8377
Dewald Haasbroek <dewald844@gmail.com>
2025-02-12 21:09:01 +0200

    Cleanup code

--- 2017/Code/day_1.cpp
+++ 2017/Code/day_1.cpp
@@ -26,7 +26,8 @@ int calculate_captcha_part_two (std::string input){
             if (input[wrapped_index] == input[i]){
                 total += input[i] - '0';
             }
-        } else {
+        } 
+        else {
             if (input[index_to_match] == input[i]){
                 total += input[i] - '0';
             }
@@ -37,36 +38,19 @@ int calculate_captcha_part_two (std::string input){
 }
 
 void part_two () {
-
     std::string data = read_file("./Data/day_1.txt");
-
-    std::cout << "_______________ Part 2 ________________" << std::endl;
-    std::cout << "Example 1 :> " << calculate_captcha_part_two ("1212") << std::endl;
-    std::cout << "Example 2 :> " << calculate_captcha_part_two ("1221") << std::endl;
-    std::cout << "Example 3 :> " << calculate_captcha_part_two ("123425") << std::endl;
-    std::cout << "Example 4 :> " << calculate_captcha_part_two ("123123") << std::endl;
-    std::cout << "Example 5 :> " << calculate_captcha_part_two ("12131415") << std::endl;
-    std::cout << "Part 2    :> " << calculate_captcha_part_two (data) << std::endl;
+    std::cout << "| Part 2 > " << calculate_captcha_part_two (data) << std::endl;
 }
 
 
 
 void part_one () {
     std::string data = read_file("./Data/day_1.txt");
-    
-    std::cout << "_______________ Part 1 ________________" << std::endl;
-    std::cout << "Example 1 :> " << calculate_captcha_part_one ("1122") << std::endl;
-    std::cout << "Example 2 :> " << calculate_captcha_part_one ("1111") << std::endl;
-    std::cout << "Example 3 :> " << calculate_captcha_part_one ("1234") << std::endl;
-    std::cout << "Example 4 :> " << calculate_captcha_part_one ("91212129") << std::endl;
-
-    std::cout << "Part 1    :> " << calculate_captcha_part_one (data) << std::endl;
+    std::cout << "| Part 1 > " << calculate_captcha_part_one (data) << std::endl;
 }
 
 int main () {
-    
     part_one();
     part_two();
-
     return 0;
 }
