34b30acf85ad423543b7625ec9836e17489ee6d7
Dewald Haasbroek <dewald844@gmail.com>
2025-02-12 21:02:33 +0200

    Day 1 complete

--- 2017/Code/day_1.cpp
+++ 2017/Code/day_1.cpp
@@ -17,16 +17,18 @@ int calculate_captcha_part_one (std::string input){
 int calculate_captcha_part_two (std::string input){
     int total = 0;
 
-    for (int i = 0 ; i < input.length(); ++ i) {
-        int index_to_match = (input.length() / 2) + i;
-        if (index_to_match > input.length()) {
+    int step = input.length() / 2;
+
+    for (int i = 0 ; i < input.length(); ++ i){
+        int index_to_match = step + i;
+        if (index_to_match >= input.length()) {
             int wrapped_index = index_to_match - input.length();
-            if (input[wrapped_index == input[i]]){
-                total += input[i];
+            if (input[wrapped_index] == input[i]){
+                total += input[i] - '0';
             }
         } else {
             if (input[index_to_match] == input[i]){
-                total += input[i];
+                total += input[i] - '0';
             }
         }
     }
@@ -35,7 +37,16 @@ int calculate_captcha_part_two (std::string input){
 }
 
 void part_two () {
+
+    std::string data = read_file("./Data/day_1.txt");
+
+    std::cout << "_______________ Part 2 ________________" << std::endl;
     std::cout << "Example 1 :> " << calculate_captcha_part_two ("1212") << std::endl;
+    std::cout << "Example 2 :> " << calculate_captcha_part_two ("1221") << std::endl;
+    std::cout << "Example 3 :> " << calculate_captcha_part_two ("123425") << std::endl;
+    std::cout << "Example 4 :> " << calculate_captcha_part_two ("123123") << std::endl;
+    std::cout << "Example 5 :> " << calculate_captcha_part_two ("12131415") << std::endl;
+    std::cout << "Part 2    :> " << calculate_captcha_part_two (data) << std::endl;
 }
 
 
@@ -43,6 +54,7 @@ void part_two () {
 void part_one () {
     std::string data = read_file("./Data/day_1.txt");
     
+    std::cout << "_______________ Part 1 ________________" << std::endl;
     std::cout << "Example 1 :> " << calculate_captcha_part_one ("1122") << std::endl;
     std::cout << "Example 2 :> " << calculate_captcha_part_one ("1111") << std::endl;
     std::cout << "Example 3 :> " << calculate_captcha_part_one ("1234") << std::endl;
